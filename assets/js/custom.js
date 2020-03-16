2c91378d2cfba7d1eb0e845f838a43ff41a7974b
Marco Pernpruner <mpernpruner@fbk.eu>
2020-03-16 11:35:58 +0100

    Upload TOPS2020 complementary material, update CSS and JS

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 $('a').each(function() {
     var a = new RegExp('/' + window.location.host + '/');
-    if(!a.test(this.href)) {
+    if(!a.test(this.href) && this.href != "" && this.href != "#") {
         $(this).click(function(event) {
             event.preventDefault();
             event.stopPropagation();
